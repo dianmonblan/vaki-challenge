f2666b515d416c05896e88c9d48ed1d0bc3f058d
Diego <dianmonbla@gmail.com>
2021-01-17 22:26:33 -0500

    Integración de asignación tipo de dato boleano sobre la interfaz de configuración libs/configuration/src/lib/interfaces.ts. Importación de módulo AngularFireModule con la respectiva configuración de Firebase desde environment.google.firebase.

--- apps/vakers/src/app/app.module.ts
+++ apps/vakers/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
+// CUSTOM COMPONENTS
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AngularFireModule.initializeApp(environment.google.firebase)
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
